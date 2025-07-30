8c7971bb53bf472d634cd426bffea419651128d2
Abhishek Singh <rathoreabhishek006@gmail.com>
2025-07-31 00:11:55 +0530

    corecting vite-config file and 404 img path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { webcrypto } from "crypto"; // ✅ add this
+
+globalThis.crypto = webcrypto; // ✅ patch for Netlify
 
 export default defineConfig({
   plugins: [react()],
